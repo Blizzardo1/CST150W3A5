6295b02dc0570117a5d62593885c9234e9ccdfa2
Adonis Deliannis <Blizzardo1@Blizzeta.net>
2021-08-22 22:42:19 -0700

    Add project files.

---
+++ CST150W3A5/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace CST150W3A5 {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && ( components != null )) {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.outputTxt = new System.Windows.Forms.TextBox();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputTxt
+            // 
+            this.outputTxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputTxt.Location = new System.Drawing.Point(9, 12);
+            this.outputTxt.Multiline = true;
+            this.outputTxt.Name = "outputTxt";
+            this.outputTxt.Size = new System.Drawing.Size(782, 250);
+            this.outputTxt.TabIndex = 0;
+            // 
+            // openBtn
+            // 
+            this.openBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.openBtn.Location = new System.Drawing.Point(9, 266);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(75, 23);
+            this.openBtn.TabIndex = 1;
+            this.openBtn.Text = "Open...";
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputTxt);
+            this.Controls.Add(this.openBtn);
+            this.Name = "Form1";
+            this.Text = "Week 3 Assignment 5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox outputTxt;
+        private Button openBtn;
+    }
+}
---
+++ CST150W3A5/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace CST150W3A5 {
+    public partial class Form1 : Form {
+        private static readonly char[] Vowels = "aeiou".ToCharArray();
+        private string _contents;
+        public Form1() {
+            InitializeComponent();
+            outputTxt.ReadOnly = true;
+        }
+
+        private static string MostVowels(IEnumerable< string > array) {
+            int mostVowels = 0;
+            int i;
+            string word = "";
+            foreach (string s in array) {
+                i = s.Count(c => Vowels.Any(x => x == c));
+                if (i <= mostVowels) continue;
+                mostVowels = i;
+                word = s;
+            }
+            return word;
+        }
+
+
+        private static string LongestWord(IReadOnlyList< string > array) {
+            string longest = array[0];
+            foreach (string t in array) {
+                if (t.Length > longest.Length)
+                    longest = t;
+            }
+
+            return longest;
+        }
+
+        private bool OpenFile() {
+            OpenFileDialog ofd = new() {
+                Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+            };
+
+
+            if (ofd.ShowDialog() == DialogResult.OK) {
+                _contents = File.ReadAllText(ofd.FileName);
+            }
+
+            if (_contents == null) {
+                throw new NullReferenceException("Must select ");
+            }
+            // Contents loaded
+            return true;
+        }
+
+        private void DisplayAndWriteOutput() {
+            // To Lower
+            string lower = _contents.ToLower();
+            string[] array = lower.Split();
+            // First and last word
+            ( string first, string last ) = ( array.First(), array.Last() );
+            string longestWord = LongestWord(array);
+            string mostVowels = MostVowels(array);
+            var sb = new StringBuilder();
+            sb.AppendLine($"First word: {first}");
+            sb.AppendLine($"Last word: {last}");
+            sb.AppendLine($"Longest word: {longestWord}");
+            sb.AppendLine($"Word with most vowels: {mostVowels}");
+            sb.AppendLine($"\n\nContent: {_contents}");
+            outputTxt.Text = sb.ToString();
+            File.WriteAllText("output.txt", sb.ToString());
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            if (!OpenFile())
+            {
+                MessageBox.Show(this, "Cannot open file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk,
+                                MessageBoxDefaultButton.Button1);
+                return;
+            }
+
+            DisplayAndWriteOutput();
+        }
+    }
+}
+
+/*
+    * 1. Converts all the words to lower-case.
+    * 2. Finds the first and last word alphabetically.
+    * 3. Finds the longest word.
+    * 4. Finds the word with the most vowels.
+    *    Display the above statistics in a text box and write the above statistics to a file; include a screenshot of your program running with successful output.
+ */
